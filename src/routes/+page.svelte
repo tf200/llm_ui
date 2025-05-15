cfc0c92c265740add9f6f5b7cce3a58edce70b7e
Taha Farjia <tfarjia@miclamor.local>
2025-05-15 12:53:59 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
 <script>
   import Chat from "$lib/components/llmChat/Chat.svelte";
+  import { goto } from "$app/navigation";
 
+  function navigateToUpload() {
+    goto("/upload");
+  }
 </script>
-<Chat></Chat>
+
+<div class="flex flex-col">
+  <div class="p-4">
+    <button
+      onclick={navigateToUpload}
+      class="bg-gray-600 hover:bg-gray-700 text-white px-4 py-2 rounded transition-colors"
+    >
+      Go to Upload
+    </button>
+  </div>
+  <Chat />
+</div>
